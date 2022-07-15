4e3a98cdd41052f4c5ab35f874d37c08ac9200b5
Dean Taylor <deanrtaylor@hotmail.com>
2022-07-15 10:26:00 +0700

    built hamburger for small screens

--- script.js
+++ script.js
@@ -2,9 +2,39 @@
 const nslider =  handle.closest(".gallery").querySelector(".slider");
 let nsliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index")
     )*/
+const hamburger = document.querySelector(".hamburger");
 let indexValue = 1;
 let width = window.innerWidth;
 console.log(width < 551);
+
+hamburger.addEventListener('click', e => {
+    navBar();
+});
+
+window.addEventListener('resize', (event) => {
+    let width = window.innerWidth;
+    let x = document.querySelector(".navsmall");
+    if(width > 550){
+        x.style.display = "none";
+         x.style.opacity = "0";
+
+    }
+});
+
+function navBar() {
+    let x = document.querySelector(".navsmall");
+    let opacity = window.getComputedStyle(x).getPropertyValue("opacity");
+    console.log(opacity);
+
+    if (x.style.display === "inline-block") {
+      x.style.display = "none";
+      x.style.opacity = "0";
+
+    } else {
+      x.style.display = "inline-block";
+      x.style.opacity = "1";
+    }
+  }
   
 
 document.addEventListener("click", e => {
