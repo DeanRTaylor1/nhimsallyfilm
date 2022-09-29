106569ec68e97d25a94313c04bbf0fb790b72515
Dean Taylor <deanrtaylor@hotmail.com>
2022-09-29 12:47:43 +0700

    added large screen navigation

--- src/Pages/Components/NavBar.tsx
+++ src/Pages/Components/NavBar.tsx
@@ -5,7 +5,10 @@ import { IconContext } from "react-icons";
 const Navbar: React.FC = () => {
   return (
     <div className="flex justify-between items-center p-2">
-      <div className="font-medium text-2xl z-10">NhimSally</div>
+      <div className="font-medium text-2xl z-10 hover:cursor-pointer">
+        NhimSally
+      </div>
+      {/* Mobile Nav */}
       <div className="group">
         <IconContext.Provider
           value={{ color: "rgb(24 24 27)", className: "global-class-name" }}
@@ -19,6 +22,14 @@ const Navbar: React.FC = () => {
             <li className="navbarDropdownListItem">Contact</li>
           </ul>
         </div>
+      </div>
+      {/* Tablet and desktop Nav */}
+      <div className="hidden md:flex">
+        <ul className="flex gap-4 ">
+          <li className="navbarStandardListItem">Galleries</li>
+          <li className="navbarStandardListItem">About Me</li>
+          <li className="navbarStandardListItem">Contact</li>
+        </ul>
       </div>
     </div>
   );
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./Components/NavBar";
+import MainSwiper from "./Components/swiper";
 
 const Home: React.FC = () => {
   return (
     <div className="grid h-screen w-screen grid-cols-1 grid-rows-layout">
       <Navbar />
-      <div className="flex justify-center items-center">Body</div>
+      <div className="flex justify-center items-center p-4"></div>
       <div className="flex justify-center items-center">Footer</div>
     </div>
   );
