1e0c8a9375eb4d47cc7f5265a42d46eabb138466
Dean Taylor <deanrtaylor@hotmail.com>
2022-09-29 12:30:58 +0700

    created mobile navigation

--- src/Pages/Components/NavBar.tsx
+++ src/Pages/Components/NavBar.tsx
@@ -5,11 +5,21 @@ import { IconContext } from "react-icons";
 const Navbar: React.FC = () => {
   return (
     <div className="flex justify-between items-center p-2">
-      <IconContext.Provider
-        value={{ color: "rgb(24 24 27)", className: "global-class-name" }}
-      >
-        <GiHamburgerMenu className="hamburger" />
-      </IconContext.Provider>
+      <div className="font-medium text-2xl z-10">NhimSally</div>
+      <div className="group">
+        <IconContext.Provider
+          value={{ color: "rgb(24 24 27)", className: "global-class-name" }}
+        >
+          <GiHamburgerMenu className="hamburger" />
+        </IconContext.Provider>
+        <div className="navbarDropdownList">
+          <ul className="flex flex-col gap-4 pt-12">
+            <li className="navbarDropdownListItem">Galleries</li>
+            <li className="navbarDropdownListItem">About Me</li>
+            <li className="navbarDropdownListItem">Contact</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
