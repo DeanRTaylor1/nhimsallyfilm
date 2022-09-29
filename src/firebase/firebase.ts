b49804924b2fcc80dffe44c2b1102f676ab350ed
Dean Taylor <deanrtaylor@hotmail.com>
2022-09-29 14:29:06 +0700

    refactored code

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -14,7 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db, app };
