3255111b9a58d2b45de83237fbec717000b9d566
Dean Taylor <deanrtaylor@hotmail.com>
2022-10-09 14:06:02 +0700

    removed unused dependencies

--- src/Pages/IndividualGallery.tsx
+++ src/Pages/IndividualGallery.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-import { Link } from "react-router-dom";
-
 import { collection, getDocs, query, limit } from "firebase/firestore";
 import { db } from "../firebase/firebase";
 
